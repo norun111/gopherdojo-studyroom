25977b6ca82d3e54ab5fb810ebcdd122c9babede
tomoyaueno29 <u.t012807@gmail.com>
2020-08-11 18:57:52 +0900

    Update cvt.go

--- kadai1/norun111/cvt/cvt.go
+++ kadai1/norun111/cvt/cvt.go
@@ -1,5 +1,13 @@
 package cvt
 
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+
+
 type Cvt struct {
 	RootDir string
 	BefExt string
@@ -13,3 +21,29 @@ func New() *Cvt {
 		AftExt: "png",
 	}
 }
+
+func (cvt *Cvt) Run() error {
+	cvt.SetFlag()
+
+	return nil
+}
+
+func (cvt *Cvt) SetFlag() error {
+
+	fs := flag.NewFlagSet(os.Args[0], flag.ExitOnError)
+
+	var (
+		bx = fs.String("b", "jpeg", "file extension before executing")
+		ax = fs.String("a", "png", "file extension after executing")
+	)
+
+	arg1 := os.Args[1]
+	fs.Parse(os.Args[2:])
+
+	fmt.Println("dir:", arg1)
+	fmt.Println("arg1:", *bx)
+	fmt.Println("arg2:", *ax)
+
+	return nil
+}
+
